b19265aff179fc5a84ecc1f59dc7c02b256b8638
Dpka_rv <97511874+DeepikaClara@users.noreply.github.com>
2022-03-28 00:29:57 +0530

    Delete spring-boot-crud-operation/Using H2 database directory

--- spring-boot-crud-operation/Using H2 database/courierdetails/repository/CourierRepository.java
+++
@@ -1,9 +0,0 @@
-package com.java.courierdetails.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.java.courierdetails.model.CourierDetails;
-
-public interface CourierRepository extends CrudRepository<CourierDetails,String> {
-
-}
