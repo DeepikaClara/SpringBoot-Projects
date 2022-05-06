c8d8a8ce2a01b946cfb155a0adb43f4ac28f1dbb
Dpka_rv <97511874+DeepikaClara@users.noreply.github.com>
2022-05-06 15:19:34 +0530

    Delete kvgschoolapp directory

--- kvgschoolapp/src/main/java/com/schoolmanagement/kvgschoolapp/repositories/X_ScoreRepository.java
+++
@@ -1,9 +0,0 @@
-package com.schoolmanagement.kvgschoolapp.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.schoolmanagement.kvgschoolapp.models.X_Score;
-
-public interface X_ScoreRepository extends JpaRepository<X_Score,Integer>{
-
-}
--- kvgschoolapp/src/main/java/com/schoolmanagement/kvgschoolapp/repositories/X_StdRepository.java
+++
@@ -1,9 +0,0 @@
-package com.schoolmanagement.kvgschoolapp.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.schoolmanagement.kvgschoolapp.models.X_Std;
-
-public interface X_StdRepository extends JpaRepository<X_Std,String>{
-
-}
