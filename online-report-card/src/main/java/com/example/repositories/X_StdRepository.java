3f0f1fe26e26294df3d7b2b3364f0810615ec202
Dpka_rv <97511874+DeepikaClara@users.noreply.github.com>
2022-05-06 15:19:07 +0530

    Delete online-report-card directory

--- online-report-card/src/main/java/com/example/repositories/X_StdRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.models.X_Std;
-
-public interface X_StdRepository extends JpaRepository <X_Std,String>{
-
-}
--- online-report-card/src/main/java/com/example/repositories/X_StudentsScoresRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.models.X_StudentsScores;
-
-public interface X_StudentsScoresRepository extends JpaRepository<X_StudentsScores,Long>{
-
-}
